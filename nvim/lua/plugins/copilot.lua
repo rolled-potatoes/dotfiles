3373af1ce1c73f6b50f5fbaa28a77399d2ed22d1
rolled-potatoes <keep4404@gmail.com>
2026-08-13 21:30:25 +0900

    feat: Copilot 제안 수락 키 분리

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,3 +1,13 @@
 return {
-  "github/copilot.vim"
+  "github/copilot.vim",
+  init = function()
+    vim.g.copilot_no_tab_map = true
+
+    vim.keymap.set("i", "<C-]>", 'copilot#Accept("\\<C-]>")', {
+      expr = true,
+      replace_keycodes = false,
+      silent = true,
+      desc = "Copilot: 제안 수락",
+    })
+  end,
 }
